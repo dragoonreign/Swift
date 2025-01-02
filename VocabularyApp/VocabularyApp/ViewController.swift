7a318d64325e5005da0f00ee4507cc67beb99523
Junji Otsuka <jun531@gmail.com>
2025-01-02 10:04:24 -1000

    First Commit

---
+++ VocabularyApp/VocabularyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VocabularyApp
+//
+//  Created by Jun on 1/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
